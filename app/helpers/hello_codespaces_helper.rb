943280645f96d8160783c30becc1da39586758c8
LuisAngelTgn <82842998+LuisAngelTgn@users.noreply.github.com>
2023-03-04 18:48:38 +0000

    Pending changes exported from your codespace

--- app/helpers/hello_codespaces_helper.rb
+++ app/helpers/hello_codespaces_helper.rb
@@ -1,2 +1,15 @@
 module HelloCodespacesHelper
+    def suma_factorial(n)
+        result = 0
+        factorial = 1
+        output = ""
+        (1..n).each do |i|
+          factorial *= i
+          result += factorial
+          (n-i+1).times do |j|
+            output <<"#{j+1}#{j == n-i ? '<br>' : ' + '}"
+          end
+        end
+        output.html_safe
+      end
 end
